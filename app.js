3cb4f979d233bb4600cfc340a2a4388d423fc0e5
level7seven <63902601+level7seven@users.noreply.github.com>
2020-07-09 15:37:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
 (function() {
-    var firebaseConfig = {
-        apiKey: "AIzaSyDXzYBrzEiokZaFApZs14L5RxWuxx1TJdM",
-        authDomain: "fir-node-2027f.firebaseapp.com",
-        databaseURL: "https://fir-node-2027f.firebaseio.com",
-        projectId: "fir-node-2027f",
-        storageBucket: "fir-node-2027f.appspot.com",
-        messagingSenderId: "865807851567",
-        appId: "1:865807851567:web:c89f316ba044c1b1d926da",
-        measurementId: "G-MLH8C669P0"
-    };
+
     firebase.initializeApp(firebaseConfig);
 
     // Get elements
